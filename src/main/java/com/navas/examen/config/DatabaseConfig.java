0b92d789d946cc7d0e9561faae66e9f6509d094b
danielnavasbecerra <danielnavasb14@gmail.com>
2024-07-26 19:17:33 -0500

    feat: :bug: Se corrige conexion a base de datos

--- src/main/java/com/navas/examen/config/DatabaseConfig.java
+++ src/main/java/com/navas/examen/config/DatabaseConfig.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     // attributes
     private static final String URL = "jdbc:mysql://localhost:3306/sgpzf";
-    private static final String USER = "campus2023";
-    private static final String PASSWORD = "campus2023";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
 
     // method to get connection
     public static Connection getConnection() throws SQLException {
